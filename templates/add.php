952d3c30a21dd4f405d8eff20422cd9948395dd1
getch <getahunlast@gmail.com>
2023-11-16 02:34:58 -0800

    add file

---
+++ templates/add.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	
+	<?php include('templates/header.php'); ?>
+
+    <section class="container gray">
+        <h1>Add Pizza</h1>
+        <form action="" method="post">
+            <label for=""></label>
+        </form>
+
+    </section>
+
+	<?php include('templates/footer.php'); ?>
+
+</html>
